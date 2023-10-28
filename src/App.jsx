d79dc25591edac6a12e0f67054ac2cb89db2e3c0
Mithilesh <mithileshchirtani@gmail.com>
2023-10-28 16:05:17 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import "./App.css";
+import SignUp from "./components/SignUp";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LogIn from "./components/LogIn";
+import ViewTable from "./components/ViewTable";
+import EditPage from "./components/EditPage";
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />} />
+        <Route path="/view" element={<ViewTable />} />
+        <Route path="/edit/:id" element={<EditPage />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
